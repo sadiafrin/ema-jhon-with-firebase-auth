f396836edd12e93d25bb06857029b514c46e9e58
sadiafrin <sadiafrin328@gmail.com>
2023-09-02 11:03:53 +0600

    cart color change

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,7 +6,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
     return (
         <nav className='header'>
-            <img src={logo} alt="" />
+            {/* <img src={logo} alt="" /> */}
+            <h2>OnlineShopBD</h2>
             <div>
                 <Link to="/">Shop</Link>
                 <Link to="/orders">Orders</Link>
